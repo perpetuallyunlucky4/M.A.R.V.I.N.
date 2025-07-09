b54cf06c666e05ca65b4041fc940eae129f162f7
perpetuallyunlucky4 <mathloverqubed@gmail.com>
2025-07-09 10:31:18 +0800

    Delete web/plugin_base.py

--- web/plugin_base.py
+++
@@ -1,6 +0,0 @@
-class Plugin:
-    def can_handle(self, message: str) -> bool:
-        raise NotImplementedError
-    
-    def handle(self, message: str) -> list[str]:
-        raise NotImplementedError
