22c78f0eccbfc4eee3c30dd8bce7d0ffbd2c1dcd
perpetuallyunlucky4 <mathloverqubed@gmail.com>
2025-07-11 14:36:04 +0800

    Update runner.py

--- web/runner.py
+++ web/runner.py
@@ -13,13 +13,13 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 #model_path="/home/pi/llama.cpp/models/codellama-7b-instruct.Q4_K_M.gguf"
 model_path = "/home/pi/llama.cpp/models/Nous-Hermes-2-Mistral-7B-DPO.Q4_K_M.gguf"
 
-max_history = 10
+max_history = 8
 
 llm = Llama(model_path,
             n_threads=4,
             chat_format="chatml",
             verbose=False,
-            n_ctx=2048
+            n_ctx=4096
         )
 
 history_path = "history.json"
@@ -30,13 +30,16 @@ if os.path.exists(history_path):
 else:
     print("no chat history!")
 
+def get_history():
+    return chat_history
+
 def generate_chat_completion(message):
     global chat_history
     chat_history.append({
         "role" : "user",
         "content" : message,
     })
-                
+
     stream = llm.create_chat_completion(messages=chat_history,
                                         stream=True,
                                         max_tokens=1000,
@@ -44,7 +47,8 @@ def generate_chat_completion(message):
                                         top_p=0.7,
                                         top_k=40
                                     )
-        
+
+    yield "__END__"
     marvin_reply = ""
     for chunk in stream:
         token = chunk["choices"][0]["delta"].get("content", "")
@@ -56,9 +60,6 @@ def generate_chat_completion(message):
         "role" : "assistant",
         "content" : marvin_reply.strip()
     })
- 
-    if len(chat_history) > (2 * max_history + 1):
-        chat_history = [chat_history[0]] + chat_history[-(max_history * 2):]
         
     plugin_responses = pm.run_plugins(marvin_reply)
     if plugin_responses:
@@ -69,12 +70,10 @@ def generate_chat_completion(message):
             })
             chat_history.append({
                 "role": "user",
-                "content": f"marvin, the plugin returned this: {response.strip()}\n. Please summarize it within a few sentences, taking into account what the I previously asked for."
+                "content": f"marvin, the plugin returned this:\n{response.strip()}\n. Please summarize it within a few sentences, answering my question:\n{message}"
             })
             
-            for token in response.split():
-                time.sleep(0.5)
-                yield token
+            yield response
             
             summary_stream = llm.create_chat_completion(messages=chat_history,
                                         stream=True,
@@ -95,12 +94,22 @@ def generate_chat_completion(message):
                 "role": "assistant",
                 "content": summary.strip()
             })
+    if len(chat_history) > (2 * max_history + 1):
+        chat_history = [chat_history[0]] + chat_history[-(max_history * 2):]
     
     with open(history_path, "w") as file:
         json.dump(chat_history, file, indent=2)
 
 
 def test_tokens(dummy):
+    chat_history = [ {
+    "role": "system",
+    "content": "2025-07-11 13:02:10.556991"
+  },
+  {
+    "role": "user",
+    "content": "marvin, the plugin returned this: 2025-07-11 13:02:10.556991\n. Please summarize it within a few sentences, taking into account what the I previously asked for."
+  }]
     reply = "This is a test of the WEB UI's token streaming capabilities [WRITE_PY]print('hhi')[/WRITE_PY] [TIME] [WEATHER] singapore [/WEATHER]"
     for token in reply.split():
         time.sleep(0.5) 
